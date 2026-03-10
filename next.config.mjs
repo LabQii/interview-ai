4959a91b66b7674294b85704c18ca10c3f0d0643
labqii <sisteminformasi2006@gmail.com>
2026-03-10 19:39:23 +0700

    fix(build): remove standalone output mode to fix nixpacks worker deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
+    // output: "standalone", // Disabled for Nixpacks to retain the workers/ folder
     eslint: {
         ignoreDuringBuilds: true,
     },
